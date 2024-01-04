af9aeca84875eb7f3f3c7e7aad071a5d60cfeb92
이찬혁 <109561938+dogbokchif@users.noreply.github.com>
2024-01-04 18:23:32 +0900

    spring init

---
+++ src/main/java/com/alt/altbackendassignment/AltBackendAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.alt.altbackendassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AltBackendAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AltBackendAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alt/altbackendassignment/AltBackendAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alt.altbackendassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AltBackendAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
